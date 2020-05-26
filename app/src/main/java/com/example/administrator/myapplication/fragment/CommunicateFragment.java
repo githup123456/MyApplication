a548d2af79cbb6c0e84bf0afc9f93ebdc0b67ece
zys <1500489973@qq.com>
2020-05-26 16:54:48 +0800

    添加对应xml文件

--- app/src/main/java/com/example/administrator/myapplication/fragment/CommunicateFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/fragment/CommunicateFragment.java
@@ -1,10 +1,13 @@
 package com.example.administrator.myapplication.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.myapplication.R;
 
@@ -12,6 +15,10 @@ import com.example.administrator.myapplication.R;
  * 通话
  */
 public class CommunicateFragment extends Fragment {
+
+    private TextView tv_top;
+    private ImageView img_right_top;
+
 
     public CommunicateFragment() {
         // Required empty public constructor
@@ -23,5 +30,14 @@ public class CommunicateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_communicate, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    //初始化view
+    private void initView(View view){
+
     }
 }
--- app/src/main/java/com/example/administrator/myapplication/fragment/ModeFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/fragment/ModeFragment.java
@@ -1,27 +1,60 @@
 package com.example.administrator.myapplication.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.administrator.myapplication.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ModeFragment extends Fragment {
-
+public class ModeFragment extends Fragment implements View.OnClickListener{
+    private Button btn_hostory;
+    private LinearLayout lly_b_list,lly_w_list;
     public ModeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mode, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    private void initView(View view){
+        btn_hostory = (Button)view.findViewById(R.id.bw_btn_problem);
+        lly_w_list = (LinearLayout) view.findViewById(R.id.bw_lly_white_list);
+        lly_b_list = (LinearLayout)view.findViewById(R.id.bw_lly_black_list);
+        btn_hostory.setOnClickListener(this);
+        lly_b_list.setOnClickListener(this);
+        lly_w_list.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bw_lly_black_list:{
+                break;
+            }
+            case R.id.bw_lly_white_list:{
+                break;
+            }
+            case R.id.bw_btn_problem:{
+                break;
+            }
+        }
     }
 }
