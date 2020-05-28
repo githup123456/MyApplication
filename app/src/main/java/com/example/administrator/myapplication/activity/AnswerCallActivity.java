e4049f1940ad498ea75d79517478aed07c453a64
“已 <m13121996913@163.com>
2020-05-28 13:50:07 +0800

    添加语音通话功能

---
+++ app/src/main/java/com/example/administrator/myapplication/activity/AnswerCallActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.myapplication.R;
+
+/**
+ * 接听界面
+ */
+public class AnswerCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_call);
+    }
+}
