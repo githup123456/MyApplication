98e2dbbdbdf700fb13dc481742ab446a2d1056da
“已 <m13121996913@163.com>
2020-05-29 08:08:51 +0800

    添加注册，登录，重置密码等

---
+++ app/src/main/java/com/example/administrator/myapplication/login/ResetPasswordActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.myapplication.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.myapplication.R;
+
+/**
+ * 重置密码
+ */
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/login/VerifyLoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.myapplication.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.myapplication.R;
+
+/**
+ * 验证码登录
+ */
+public class VerifyLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_login);
+    }
+}
